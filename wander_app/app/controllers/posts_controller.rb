1d5b4132d9a4259b4f4d57cec590147e88eda050
Mala <m.nimalasuriya@gmail.com>
2015-12-10 16:14:59 -0500

    full CRUD complete for posts

--- wander_app/app/controllers/posts_controller.rb
+++ wander_app/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  include PostsHelper
 
   def index
     @posts = Post.all
@@ -6,7 +7,44 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    #@posts = @user.posts - to make sure only a specific user's posts show up
   end
-  
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+   @post = Post.new(post_params)
+   @post.save
+     redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to posts_path
 end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+      # flash.notice = "Post/Food Log Deleted" - maybe include. Distracting?
+      redirect_to posts_path
+  end
+
+
+
+
+
+
+
+
+
+
+
+end # end of class
